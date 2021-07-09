5dbf853c7278e312d4afc610e91e087d000ba950
sudo-Archer <archernphillips@gmail.com>
2021-07-09 23:28:12 +0700

    init with spring (Spring Boot DevTool, Lombok, Spring Web)

---
+++ src/test/java/upsidedowncubes/multiplayerzork/MultiplayerZorkApplicationTests.java
@@ -0,0 +1,13 @@
+package upsidedowncubes.multiplayerzork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplayerZorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
