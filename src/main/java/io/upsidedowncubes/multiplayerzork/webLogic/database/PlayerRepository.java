9275ae5f407bc1897e0821271ad178054444da7e
Pornkamol Luthra <suhavni@gmail.com>
2021-07-20 23:39:08 +0700

    added a method for potential item impl

--- src/main/java/io/upsidedowncubes/multiplayerzork/webLogic/database/PlayerRepository.java
+++ src/main/java/io/upsidedowncubes/multiplayerzork/webLogic/database/PlayerRepository.java
@@ -3,9 +3,10 @@ package io.upsidedowncubes.multiplayerzork.webLogic.database;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface PlayerRepository extends CrudRepository<PlayerEntity, String> {
     PlayerEntity findByUsername(String username);
+    Set<PlayerEntity> findAllBySessionIDAndRowAndCol(String sessionID, int row, int col);
 }
